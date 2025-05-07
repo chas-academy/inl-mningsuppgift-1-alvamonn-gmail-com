75a3b577b58685048127fabbb80b2117c90eb414
alvamonn@gmail.com <alvamonn@gmail.com>
2025-05-07 15:32:54 +0200

    For-loop and initializations

--- main.c
+++ main.c
@@ -8,6 +8,20 @@ int main()
 
    
     // Your code goes here
+    int randnums[100];
+
+    for (int i = 0; i < 100; i++){
+        randnums[i] = (rand() % 6) + 1;
+    }
+
+    int ones = 0;
+    int twos = 0;
+    int threes = 0;
+    int fours = 0;
+    int fives = 0;
+    int six = 0;
+    int sum = 0;
+    
 
     return 0;
 }
