a4ce655d727c18d05c29ef328b206f716af208d3
alvamonn@gmail.com <alvamonn@gmail.com>
2025-05-07 15:56:59 +0200

    exercise done

--- main.c
+++ main.c
@@ -21,6 +21,42 @@ int main()
     int fives = 0;
     int six = 0;
     int sum = 0;
+
+    for (int i = 0; i < 100; i++){
+        if (randnums[i] == 1){
+            ones++;
+        }
+        if (randnums[i] == 2){
+            twos++;
+        }
+        if (randnums[i] == 3){
+            threes++;
+        }
+        if (randnums[i] == 4){
+            fours++;
+        }
+        if (randnums[i] == 5){
+            fives++;
+        }
+        if (randnums[i] == 6){
+            six++;
+        }
+
+        sum+=randnums[i];
+    }
+
+    float mid = sum / 100.0; 
+
+    printf("%d\n", ones);
+    printf("%d\n", twos);
+    printf("%d\n", threes);
+    printf("%d\n", fours);
+    printf("%d\n", fives);
+    printf("%d\n", six);
+    printf("%d\n", sum);
+    printf("%.1f\n", mid);
+
+
     
 
     return 0;
